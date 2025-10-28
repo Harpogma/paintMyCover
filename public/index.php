de3e2468e5160a561298291404dd6368cdc68ded
Lea Pires <lea.pires@heig-vd.ch>
2025-10-28 11:23:25 +0100

    initialisation translation/cookie V2

--- public/index.php
+++ public/index.php
@@ -1,7 +1,28 @@
-<!-- cette page permet de visualiser, supprimer, redirige aux pages -->
+<?php
+// Inclusion de l'utilitaire de chargement des traductions
+require_once __DIR__ . '/../src/i18n/load-translation.php';
+
+const COOKIE_NAME = 'lang';
+const COOKIE_LIFETIME = (30 * 24 * 60 * 60);
+const DEFAULT_LANG = 'fr';
+
+$lang = $_COOKIE[COOKIE_NAME] ?? DEFAULT_LANG;
+
+$traductions = loadTranslation($lang);
+
+// changer langue préférée
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $lang = $_POST['language'] ?? DEFAULT_LANG;
+
+    setcookie(COOKIE_NAME, $lang, time() + COOKIE_LIFETIME);
+    header('Location: index.php');
+    exit;
+}
+?>
+
 
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="<?= htmlspecialchars($lang) ?>">
 
 <head>
     <meta charset="utf-8">
@@ -9,6 +30,7 @@
     <meta name="color-scheme" content="light dark">
     <link rel="stylesheet" href="css/custom.css">
 
+    <title><?= htmlspecialchars($traductions['title']) ?></title>
     <title>Page d'accueil | PaintMyCover</title>
 </head>
 
---
+++ src/i18n/translations/fr.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'title' => "Page d'accueil | PaintMyCover",
+    
+        'languages' => [
+        'en' => 'English',
+        'fr' => 'French (Français)',
+    ],
+    'submit' => 'Change language',
+];
